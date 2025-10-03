47f5ee6daf3c9adf81d6287da8fe321914d3ace9
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-10-03 13:21:05 +0530

    prev, next button added to the quote component

--- src/components/quote/Quote.jsx
+++ src/components/quote/Quote.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import './Quote.css'
+import { FaCaretLeft } from "react-icons/fa6";
+import { FaCaretRight } from "react-icons/fa";
 
-const Quote = ({details}) => {
+const Quote = ({details,style}) => {
   return (
     <div className='quote-container'>
+      <div className='quote-prev' style={style}><FaCaretLeft /></div>
         {details?.map((detail) => (
-                <div key={detail.id} className='quote-detail'>
-                    <div className='quote-detail-tag'>{detail.tag}</div>
-                    <div className='quote-detail-quote'>{detail.quote}</div>
-                    <div className='quote-detail-by'>- {detail.by} -</div>
+                <div key={detail.id} className='quote-detail' >
+                    <div className='quote-detail-tag ' style={style}>{detail.tag}</div>
+                    <div className='quote-detail-quote' style={style}>{detail.quote}</div>
+                    <div className='quote-detail-by' style={style}>- {detail.by} -</div>
                 </div>
                 
                 )
             )
         }
+        <div className='quote-next'><FaCaretRight /></div>
         
     </div>
   )
