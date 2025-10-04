17919dc9012c1cbd4dc2adeead6f5207cef7f60f
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-10-04 13:36:10 +0530

    modified quote component

--- src/components/quote/Quote.jsx
+++ src/components/quote/Quote.jsx
@@ -1,24 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Quote.css'
-import { FaCaretLeft } from "react-icons/fa6";
-import { FaCaretRight } from "react-icons/fa";
 
-const Quote = ({details,style}) => {
+
+const Quote = ({tag,quote,by,ref,style}) => {
+
   return (
-    <div className='quote-container'>
-      <div className='quote-prev' style={style}><FaCaretLeft /></div>
-        {details?.map((detail) => (
-                <div key={detail.id} className='quote-detail' >
-                    <div className='quote-detail-tag ' style={style}>{detail.tag}</div>
-                    <div className='quote-detail-quote' style={style}>{detail.quote}</div>
-                    <div className='quote-detail-by' style={style}>- {detail.by} -</div>
-                </div>
-                
-                )
-            )
-        }
-        <div className='quote-next' style={style}><FaCaretRight /></div>
-        
+    <div className='quote-container' ref={ref} style={style}>        
+      <div className='quote-detail'  >
+          <div className='quote-detail-tag ' style={style}>{tag}</div>
+          <div className='quote-detail-quote' style={style}>{quote}</div>
+          <div className='quote-detail-by' style={style}>- {by} -</div>
+      </div>       
     </div>
   )
 }
