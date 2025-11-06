e989995d499b0710aebe867a3432666b08497525
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-11-06 16:52:22 +0530

    added addQuote functionalites, badwords prevention

--- src/components/addQuote/AddQuote.jsx
+++ src/components/addQuote/AddQuote.jsx
@@ -1,26 +1,120 @@
-import React from 'react'
-import Input from '../input/Input'
+import React, { useState } from 'react'
 import './AddQuote.css'
+import { Filter } from 'bad-words'
 
 const AddQuote = () => {
+    const [quote, setQuote] = useState("");
+    const [tags, setTags] = useState([]);
+    const [author, setAuthor] = useState("");
+    const [filterText, setFiltertext] = useState({})
+
+    const filter = new Filter();
+    const addValidation = () => {
+        let textFilter = {quote:"",tags:"",author:""};
+        let valid = true;
+        if(!quote.trim()){
+            textFilter.quote = "Quote is required!"
+            setQuote("")
+            valid= false;
+        }else if(filter.isProfane(quote)){
+            textFilter.quote = "Inappropreate language detected!"
+            const cleanQuote = filter.clean(quote)
+            setQuote(cleanQuote)
+            valid = false;
+        }
+        if(tags && tags.length>0){
+            const cleanTags = tags.map(tag=>{
+                if(filter.isProfane(tag)){
+                    textFilter.tags = "Inappropreate language detected!"
+                    valid = false;
+                }                
+                return filter.clean(tag)
+            })
+            setTags(cleanTags)         
+        }
+        if(!author.trim()){
+            textFilter.author = "Author is required!"
+            setAuthor("")
+            valid= false;
+        }else if(filter.isProfane(author)){
+            textFilter.author = "Inappropreate language detected!"
+            const cleanAuthor = filter.clean(author)
+            setAuthor(cleanAuthor)
+            valid = false;
+        }
+        setFiltertext(textFilter)
+        console.log("Valid",valid)
+        return valid;
+    }
+
+    const handleAddValidation = () => {
+        const validate = addValidation()
+        let isSuccess = true;
+        if(validate){
+            isSuccess=true
+            setQuote("")
+            setTags([])
+            setAuthor("")
+        }else{
+            addValidation()
+            isSuccess=false
+        }
+        console.log("IsSuccess",isSuccess)
+        return isSuccess;
+    }
+
+    const addSuccess = () => {
+        const success = handleAddValidation()
+        if(success){
+            console.log("Success")
+        }else{
+            console.log("fail")
+        }
+    }
+
   return (
     <div className='addquote-container'>
         <div className='addquote-title'> Add a Quote </div>
         <div className='addquote-quote'>
-            <Input/>
+           <textarea 
+                name="quote" 
+                id="" 
+                placeholder='Quote..' 
+                className='quote-txt' 
+                value={quote} 
+                onChange = {(q) => setQuote(q.target.value)}
+            ></textarea>
+            {filterText.quote && <span className='warn-message'>{filterText.quote}</span>}
         </div>
         <div className='addquote-tags'>
-            <Input/>
+           <textarea name="tags" 
+                id="" 
+                placeholder='Tags..' 
+                className='tags-txt' 
+                value={tags.join("\n")} 
+                onChange = {(t) => {
+                    const tagList = t.target.value.split("\n").map(tag => tag.trim())
+                    setTags(tagList);                    
+                }}>
+            </textarea>
+            {filterText.tags && <span className='warn-message'>{filterText.tags}</span>}
         </div>
         <div className='addquote-author'>
-            <Input/>
+            <textarea name="author" 
+                id="" 
+                placeholder='Author..' 
+                className='author-txt' 
+                value={author} 
+                onChange = {(a) => setAuthor(a.target.value)}>
+            </textarea>
+            {filterText.author && <span className='warn-message'>{filterText.author}</span>}
         </div>
         <div className='addquote-button'>
             <div className='addquote-cancel-btn'>
                 <button>Cancel</button>
             </div>
             <div className='addquote-add-btn'>
-                <button>Add</button>
+                <button onClick={addSuccess}>Add</button>
             </div>
         </div>
         
